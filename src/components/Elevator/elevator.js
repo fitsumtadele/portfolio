4b7566af7afa5b29c887005b324a944138b5d409
FitsumTadele <fitsumtadele19@gmail.com>
2024-02-18 11:38:53 +0300

    elevator pitch video

--- src/components/Elevator/elevator.js
+++ src/components/Elevator/elevator.js
@@ -19,6 +19,10 @@ function Elevator() {
           Here is my elevator  pitch that describes the purpose of this project and what I aim to achieve with it. This section also
          I am a full stack developer with experience of 3+ years . I have completed projects for eCommerce websites, social media platforms, web applications, mobile apps using
       </p>
+  
+      <iframe src="https://www.veed.io/embed/172e4d9a-1df2-4e16-9653-187e95372197" width="744" height="504" frameborder="0" title="WIN_20240217_19_30_35_Pro" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+     
+      
         
       </Container>
     </Container>
